91a67135d759180ce0c7345c6bd720088f8b892d
Jon Wendell Cabrera <144750558+JonWendell@users.noreply.github.com>
2024-04-06 14:47:42 +0800

    database

--- vet.sql
+++ vet.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Apr 04, 2024 at 12:42 PM
+-- Generation Time: Mar 12, 2024 at 02:34 PM
 -- Server version: 8.0.30
 -- PHP Version: 8.1.10
 
@@ -37,66 +37,29 @@ CREATE TABLE `appointments` (
   `updated_at` timestamp NULL DEFAULT NULL,
   `user_id` bigint UNSIGNED NOT NULL,
   `status` enum('pending','accepted','completed','canceled') CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'pending',
-  `branch_id` bigint UNSIGNED DEFAULT NULL
+  `clinic_id` bigint UNSIGNED DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Dumping data for table `appointments`
 --
 
-INSERT INTO `appointments` (`id`, `first_name`, `last_name`, `appointment_date`, `appointment_time`, `created_at`, `updated_at`, `user_id`, `status`, `branch_id`) VALUES
+INSERT INTO `appointments` (`id`, `first_name`, `last_name`, `appointment_date`, `appointment_time`, `created_at`, `updated_at`, `user_id`, `status`, `clinic_id`) VALUES
 (1, 'Erzie', 'Erzie', '2024-03-03', '08:00:00', '2024-03-02 08:08:04', '2024-03-02 08:56:04', 12, 'completed', 16),
 (2, 'Erzie', 'Erzie', '2024-03-03', '10:00:00', '2024-03-02 08:48:49', '2024-03-03 05:34:29', 12, 'completed', 16),
 (3, 'Erzie', 'Erzie', '2024-03-03', '08:00:00', '2024-03-03 05:34:51', '2024-03-03 05:47:43', 12, 'canceled', 16),
 (4, 'Erzie', 'Erzie', '2024-03-03', '08:00:00', '2024-03-03 05:50:20', '2024-03-03 05:50:46', 12, 'canceled', 16),
 (5, 'Erzie', 'Erzie', '2024-03-03', '08:00:00', '2024-03-03 05:51:44', '2024-03-03 05:52:14', 12, 'canceled', 16),
 (6, 'Erzie', 'Erzie', '2024-03-03', '10:00:00', '2024-03-03 06:27:06', '2024-03-03 06:27:44', 12, 'completed', 16),
-(7, 'Erzie', 'Erzie', '2024-03-03', '08:00:00', '2024-03-03 06:28:25', '2024-03-03 06:28:57', 12, 'canceled', 16),
-(8, 'patient', 'patient', '2024-04-03', '08:00:00', '2024-04-01 08:19:13', '2024-04-01 08:19:13', 10, 'pending', 16),
-(9, 'patient', 'patient', '2024-04-29', '17:00:00', '2024-04-03 05:58:58', '2024-04-03 06:01:25', 10, 'completed', 16),
-(10, 'patient', 'patient', '2024-04-05', '13:00:00', '2024-04-03 20:24:56', '2024-04-03 20:24:56', 10, 'pending', 18),
-(11, 'patient', 'patient', '2024-04-13', '13:00:00', '2024-04-04 02:17:37', '2024-04-04 02:17:37', 10, 'pending', 16);
+(7, 'Erzie', 'Erzie', '2024-03-03', '08:00:00', '2024-03-03 06:28:25', '2024-03-03 06:28:57', 12, 'canceled', 16);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `audits`
+-- Table structure for table `clinics`
 --
 
-CREATE TABLE `audits` (
-  `id` bigint UNSIGNED NOT NULL,
-  `inventory_id` bigint UNSIGNED NOT NULL,
-  `upc` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  `description` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  `old_quantity` int UNSIGNED NOT NULL,
-  `quantity` int UNSIGNED NOT NULL,
-  `type` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'inbound',
-  `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Dumping data for table `audits`
---
-
-INSERT INTO `audits` (`id`, `inventory_id`, `upc`, `name`, `description`, `old_quantity`, `quantity`, `type`, `created_at`, `updated_at`) VALUES
-(1, 5, '17102537969873', 'Chains', 'Dog Leash', 101, 21, 'inbound', '2024-03-12 07:50:14', '2024-03-12 07:50:14'),
-(2, 5, '17102537969873', 'Chains', 'Dog Leash', 122, 1, 'inbound', '2024-03-12 08:11:05', '2024-03-12 08:11:05'),
-(3, 5, '17102537969873', 'Chains', 'Dog Leash', 123, 21, 'inbound', '2024-03-13 05:20:52', '2024-03-13 05:20:52'),
-(9, 12, '17103376866', 'MedDog', 'Dog Med', 0, 35, 'inbound', '2024-03-13 05:48:06', '2024-03-13 05:48:06'),
-(10, 13, '171034228612', 'Leash', 'Dog Leash', 0, 35, 'inbound', '2024-03-13 07:04:46', '2024-03-13 07:04:46'),
-(11, 5, '17102537969873', 'Chains', 'Dog Leash', 144, 200, 'inbound', '2024-04-01 07:55:35', '2024-04-01 07:55:35'),
-(12, 5, '17102537969873', 'Chains', 'Dog Leash', 344, 1, 'inbound', '2024-04-03 06:11:31', '2024-04-03 06:11:31'),
-(13, 5, '17102537969873', 'Chains', 'Dog Leash', 345, 69, 'inbound', '2024-04-03 06:11:40', '2024-04-03 06:11:40');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `branches`
---
-
-CREATE TABLE `branches` (
+CREATE TABLE `clinics` (
   `id` bigint UNSIGNED NOT NULL,
   `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `location` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
@@ -104,39 +67,17 @@ CREATE TABLE `branches` (
   `status` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
-  `user_id` bigint UNSIGNED NOT NULL DEFAULT '1'
+  `user_id` bigint UNSIGNED NOT NULL DEFAULT '1',
+  `doctor_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
--- Dumping data for table `branches`
+-- Dumping data for table `clinics`
 --
 
-INSERT INTO `branches` (`id`, `name`, `location`, `contact`, `status`, `created_at`, `updated_at`, `user_id`) VALUES
-(16, 'Gwenchana Dentist', 'Santo Ninos', '09305115251', 'Active', '2024-02-19 05:30:54', '2024-04-01 08:08:06', 1),
-(18, 'Janzel Clinic', 'Cebu', '09092133212', 'Active', '2024-02-19 06:15:17', '2024-02-19 06:15:17', 1);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `carts`
---
-
-CREATE TABLE `carts` (
-  `id` bigint UNSIGNED NOT NULL,
-  `user_id` bigint UNSIGNED NOT NULL,
-  `product_id` bigint UNSIGNED NOT NULL,
-  `quantity` int NOT NULL,
-  `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL,
-  `branch_id` bigint UNSIGNED DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Dumping data for table `carts`
---
-
-INSERT INTO `carts` (`id`, `user_id`, `product_id`, `quantity`, `created_at`, `updated_at`, `branch_id`) VALUES
-(22, 10, 5, 3, '2024-04-04 02:39:49', '2024-04-04 02:39:49', 16);
+INSERT INTO `clinics` (`id`, `name`, `location`, `contact`, `status`, `created_at`, `updated_at`, `user_id`, `doctor_name`) VALUES
+(16, 'Gwenchana Dentist', 'Santo Nino', '09305115251', 'Active', '2024-02-19 05:30:54', '2024-02-19 05:50:22', 1, 'Gwencha Gwenchanayo'),
+(18, 'Janzel Clinic', 'Cebu', '09092133212', 'Active', '2024-02-19 06:15:17', '2024-02-19 06:15:17', 1, 'Janzel Bongo');
 
 -- --------------------------------------------------------
 
@@ -162,41 +103,26 @@ CREATE TABLE `failed_jobs` (
 
 CREATE TABLE `inventories` (
   `id` bigint UNSIGNED NOT NULL,
-  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  `description` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `description` text COLLATE utf8mb4_unicode_ci,
   `quantity` int NOT NULL DEFAULT '0',
-  `image` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `category` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `image` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `category` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `price` decimal(10,2) DEFAULT NULL,
-  `upc` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `upc` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `expiration` timestamp NULL DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
-  `branch_id` bigint UNSIGNED NOT NULL
+  `clinic_id` bigint UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Dumping data for table `inventories`
 --
 
-INSERT INTO `inventories` (`id`, `name`, `description`, `quantity`, `image`, `category`, `price`, `upc`, `expiration`, `created_at`, `updated_at`, `branch_id`) VALUES
-(5, 'Chains', 'Dog Leash', 414, '1710253796.jpg', 'Solid', 32.00, '17102537969873', '2024-03-11 16:00:00', '2024-03-12 14:29:00', '2024-04-03 06:11:40', 16),
-(12, 'MedDog', 'Dog Med', 35, '1710337686.jpg', 'Liquid', 35.00, '17103376866', '2024-03-12 16:00:00', '2024-03-13 13:48:00', '2024-03-13 05:48:06', 16),
-(13, 'Leash', 'Dog Leash', 35, '1710342286.png', '35', 45.00, '171034228612', '2024-03-12 16:00:00', '2024-03-13 15:04:00', '2024-03-13 07:04:46', 18);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `inventory_order`
---
-
-CREATE TABLE `inventory_order` (
-  `inventory_id` bigint UNSIGNED NOT NULL,
-  `order_id` bigint UNSIGNED NOT NULL,
-  `quantity` int NOT NULL,
-  `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+INSERT INTO `inventories` (`id`, `name`, `description`, `quantity`, `image`, `category`, `price`, `upc`, `expiration`, `created_at`, `updated_at`, `clinic_id`) VALUES
+(5, 'Chains', 'Dog Leash', 32, '1710253796.jpg', 'Solid', 32.00, '17102537969873', '2024-03-11 16:00:00', '2024-03-12 14:29:00', '2024-03-12 06:29:56', 16),
+(6, 'Dog Food', 'Food', 32, '1710253838.jpg', 'Solid', 500.00, '17102538388492', '2024-03-11 16:00:00', '2024-03-12 14:30:00', '2024-03-12 06:30:38', 18);
 
 -- --------------------------------------------------------
 
@@ -227,20 +153,7 @@ INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 (10, '2024_02_19_132112_add_doctor_name_to_clinics_table', 9),
 (11, '2024_03_02_154939_update_status_column_in_appointments_table', 10),
 (12, '2024_03_03_134341_add_canceled_status_to_appointments_table', 11),
-(13, '2024_03_12_132234_remove_clinic_id_from_inventories_table', 12),
-(14, '2024_03_12_144100_create_audits_table', 13),
-(15, '2024_03_12_151530_create_audits_table', 14),
-(16, '2024_03_12_153646_create_audits_table', 15),
-(17, '2024_04_01_144714_create_sales_table', 16),
-(18, '2024_04_01_145100_rename_clinics_table_to_branches', 17),
-(19, '2024_04_01_145348_update_foreign_keys_to_branch_id', 18),
-(20, '2024_04_01_145714_update_user_foreign_key_to_branch_id', 19),
-(21, '2024_04_01_160411_remove_doctor_name_from_branches_table', 20),
-(22, '2024_04_01_162407_add_branch_id_to_sales_table', 21),
-(23, '2024_04_01_172405_rename_product_id_column_in_sales_table', 22),
-(24, '2024_04_03_125743_create_sales_table', 23),
-(25, '2024_04_04_045217_modify_carts_table', 24),
-(26, '2024_04_04_050727_add_branch_id_to_carts_table', 25);
+(13, '2024_03_12_132234_remove_clinic_id_from_inventories_table', 12);
 
 -- --------------------------------------------------------
 
@@ -312,7 +225,7 @@ CREATE TABLE `users` (
   `password` varchar(255) NOT NULL,
   `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `branch_id` int DEFAULT NULL,
+  `clinic_id` int DEFAULT NULL,
   `role` varchar(255) NOT NULL DEFAULT 'super_admin'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
@@ -320,16 +233,14 @@ CREATE TABLE `users` (
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `username`, `firstName`, `lastName`, `middleName`, `address`, `gender`, `age`, `email`, `password`, `created_at`, `updated_at`, `branch_id`, `role`) VALUES
+INSERT INTO `users` (`id`, `username`, `firstName`, `lastName`, `middleName`, `address`, `gender`, `age`, `email`, `password`, `created_at`, `updated_at`, `clinic_id`, `role`) VALUES
 (1, 'Sett', 'Jon Wendell', 'Cabrera', 'Lontoc', 'Nacoco Santo Nino', 'male', 22, 'corvecc1@gmail.com', '$2y$12$2k8M1Dr4znqRowxeg/4LiePrDsTbOftt0NC43FwikNg0jk9IoQUCi', '2024-02-19 05:31:38', '2024-02-19 06:01:09', 16, 'super_admin'),
 (4, 'staffcal', 'staffcal', 'staffcal', 'staffcal', 'Test', 'female', 21, 'staffcal@gmail.com', '$2y$12$1xIoG7fECmJyaCvzk9hB7uMn6lh0Yt4k04a5LkaVMwlxe97cW98z6', '2024-02-19 06:13:07', '2024-02-19 06:13:07', 16, 'staff'),
 (6, 'staffvic', 'staffvic', 'staffvic', 'staffvic', 'Test', 'female', 21, 'staffvic@gmail.com', '$2y$12$PW0q/y1f8t7Lu61MSqdSeOcDvKU1t66xeoMDh3ImIFpKDKsI91av2', '2024-02-19 06:15:48', '2024-02-19 06:15:48', 18, 'staff'),
 (7, 'admincal', 'admincal', 'admincal', 'admincal', 'Test', 'female', 21, 'admincal@gmail.com', '$2y$12$xgLPf4n42RNZ4Ewog5sC8.uKW/go4WSFYehre9FExL4soV3iI/kFu', '2024-02-19 06:16:39', '2024-02-19 06:16:39', 16, 'admin'),
 (10, 'patient', 'patient', 'patient', 'patient', 'Test', 'male', 21, 'patient@gmail.com', '$2y$12$Eqdn7KFYxfowH0zsD6N3AOI0DpGh95lLHClvGyoz9C5jQIUaLNWsC', '2024-02-19 06:50:01', '2024-02-19 06:50:01', NULL, 'patient'),
 (11, 'clientcal', 'clientcal', 'clientcal', 'clientcal', 'Test', 'female', 21, 'clientcal@gmail.com', '$2y$12$MXJHFcL5KU0JqA/aaLGVbuZ5L0i9tMzNvdjbc6kL1KONuSqkWfa52', '2024-02-19 06:56:07', '2024-02-19 06:56:07', NULL, 'patient'),
-(12, 'Erzie', 'Erzie', 'Erzie', 'Erzie', 'Cebu', 'male', 21, 'janzkiemalditz@gmail.com', '$2y$12$LQHMcDEiQpLRKZVr7/0g9.6wt3/MM6tr3gbQHAVUWeYoD9Yxh2TFC', '2024-02-19 06:58:17', '2024-02-19 06:58:17', NULL, 'patient'),
-(13, 'adminvic', 'adminvic', 'adminvic', 'adminvic', 'test', 'female', 32, 'adminvic@gmail.com', '$2y$12$ln9eWedJqdIjAMTWjHJeKeVa1kDyI.stctmp/7OAzJtSK0oXazt9e', '2024-03-13 06:38:37', '2024-03-13 06:38:37', 18, 'admin'),
-(15, 'testsubject', 'testsubject', 'testsubject', 'testsubject', 'testsubject', 'male', 21, 'testsubject@gmail.com', '$2y$12$HPK9Ie1/EO6cUNl//DOJDOlP2gGgDK2rmj/ApkLov4Yt.MN1l/t6a', '2024-04-03 05:51:25', '2024-04-03 05:51:25', 16, 'admin');
+(12, 'Erzie', 'Erzie', 'Erzie', 'Erzie', 'Cebu', 'male', 21, 'janzkiemalditz@gmail.com', '$2y$12$LQHMcDEiQpLRKZVr7/0g9.6wt3/MM6tr3gbQHAVUWeYoD9Yxh2TFC', '2024-02-19 06:58:17', '2024-02-19 06:58:17', NULL, 'patient');
 
 --
 -- Indexes for dumped tables
@@ -340,27 +251,13 @@ INSERT INTO `users` (`id`, `username`, `firstName`, `lastName`, `middleName`, `a
 --
 ALTER TABLE `appointments`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `appointments_clinic_id_foreign` (`branch_id`);
+  ADD KEY `appointments_clinic_id_foreign` (`clinic_id`);
 
 --
--- Indexes for table `audits`
+-- Indexes for table `clinics`
 --
-ALTER TABLE `audits`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `audits_inventory_id_foreign` (`inventory_id`);
-
---
--- Indexes for table `branches`
---
-ALTER TABLE `branches`
+ALTER TABLE `clinics`
   ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `carts`
---
-ALTER TABLE `carts`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `carts_branch_id_foreign` (`branch_id`);
 
 --
 -- Indexes for table `failed_jobs`
@@ -415,25 +312,13 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `appointments`
 --
 ALTER TABLE `appointments`
-  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
--- AUTO_INCREMENT for table `audits`
+-- AUTO_INCREMENT for table `clinics`
 --
-ALTER TABLE `audits`
-  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
-
---
--- AUTO_INCREMENT for table `branches`
---
-ALTER TABLE `branches`
-  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
-
---
--- AUTO_INCREMENT for table `carts`
---
-ALTER TABLE `carts`
-  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+ALTER TABLE `clinics`
+  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
 --
 -- AUTO_INCREMENT for table `failed_jobs`
@@ -445,13 +330,13 @@ ALTER TABLE `failed_jobs`
 -- AUTO_INCREMENT for table `inventories`
 --
 ALTER TABLE `inventories`
-  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
+  MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT for table `personal_access_tokens`
@@ -463,7 +348,7 @@ ALTER TABLE `personal_access_tokens`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- Constraints for dumped tables
@@ -473,19 +358,7 @@ ALTER TABLE `users`
 -- Constraints for table `appointments`
 --
 ALTER TABLE `appointments`
-  ADD CONSTRAINT `appointments_clinic_id_foreign` FOREIGN KEY (`branch_id`) REFERENCES `branches` (`id`) ON DELETE SET NULL;
-
---
--- Constraints for table `audits`
---
-ALTER TABLE `audits`
-  ADD CONSTRAINT `audits_inventory_id_foreign` FOREIGN KEY (`inventory_id`) REFERENCES `inventories` (`id`) ON DELETE CASCADE;
-
---
--- Constraints for table `carts`
---
-ALTER TABLE `carts`
-  ADD CONSTRAINT `carts_branch_id_foreign` FOREIGN KEY (`branch_id`) REFERENCES `branches` (`id`) ON DELETE CASCADE;
+  ADD CONSTRAINT `appointments_clinic_id_foreign` FOREIGN KEY (`clinic_id`) REFERENCES `clinics` (`id`) ON DELETE SET NULL;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
