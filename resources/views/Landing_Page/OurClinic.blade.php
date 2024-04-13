5e28d230f76d7ad3f57985f5aa1bd70d6acfac90
cydieg <cydiegargullo@gmail.com>
2024-04-13 11:08:33 +0800

    About our shop

--- resources/views/Landing_Page/OurClinic.blade.php
+++ resources/views/Landing_Page/OurClinic.blade.php
@@ -19,9 +19,18 @@
               </span>
             </h2>
             <p>
-              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
-              industry's standard dummy text ever since theLorem Ipsum is simply dummy text of the printing and
-              typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the
+            Our shop was founded in Calapan City, Oriental Mindoro and has been a beloved part of the community since 2018. 
+            With our commitment to providing exceptional products and services for pets and their owners, we have expanded to include a branch in Roxas, Oriental Mindoro, ensuring that even more furry friends and their families have access to top-quality care and supplies.
+            </p>
+
+            <p>
+            At Rem's Pet Shop, we understand the special bond between pets and their owners. That's why we strive to offer a wide range of products, from premium pet foods to toys, accessories, and grooming supplies, carefully selected to meet the unique needs of every pet. 
+            Our knowledgeable and friendly staff are here to assist you in finding the perfect items for your furry companions.
+            Whether you're a seasoned pet parent or a first-time pet owner, we're dedicated to providing you with the support and resources you need to give your pets the happy, healthy lives they deserve. 
+            </p>
+
+            <p>
+            Visit us at either of our convenient locations in Calapan City or Roxas, and let us be your trusted partner in pet care.
             </p>
             <div>
               <a href="">
--- resources/views/back/layout/EcommerceLayout/headerfooter-layout.blade.php
+++ resources/views/back/layout/EcommerceLayout/headerfooter-layout.blade.php
@@ -139,8 +139,7 @@
   <!-- footer section -->
   <section class="container-fluid footer_section">
     <p>
-      &copy; 2019 All Rights Reserved By
-      <a href="https://html.design/">Free Html Templates</a>
+      &copy; 2024 All Rights Reserved By Rem's Pet Shop
     </p>
   </section>
   <!-- footer section -->
