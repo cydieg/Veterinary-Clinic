a68fe6c96ac9665817be5812b2fc3fad2490ddac
cydieg <cydiegargullo@gmail.com>
2024-04-15 15:39:17 +0800

    map additional

--- resources/views/mapping/map.blade.php
+++ resources/views/mapping/map.blade.php
@@ -36,12 +36,12 @@
 
         // Add a marker for Rem's Pet Shop Camilmil
         var marker1 = L.marker([13.4061, 121.1760]).addTo(map)
-            .bindPopup("<b>Rem's Pet Shop Camilmil</b><br>Located here. <br><a href='https://www.google.com/maps/@13.4055364,121.1751609,3a,75y,337.08h,90t/data=!3m7!1e1!3m5!1s-3UaGvp-WrUGA56pyk-7dg!2e0!6shttps:%2F%2Fstreetviewpixels-pa.googleapis.com%2Fv1%2Fthumbnail%3Fpanoid%3D-3UaGvp-WrUGA56pyk-7dg%26cb_client%3Dsearch.revgeo_and_fetch.gps%26w%3D96%26h%3D64%26yaw%3D337.08377%26pitch%3D0%26thumbfov%3D100!7i16384!8i8192?entry=ttu'>View on Google Maps</a>")
+            .bindPopup("<b>Rem's Pet Shop Camilmil</b><br>Available for Accomodation <br>Located here. <br><a href='https://www.google.com/maps/@13.4055364,121.1751609,3a,75y,337.08h,90t/data=!3m7!1e1!3m5!1s-3UaGvp-WrUGA56pyk-7dg!2e0!6shttps:%2F%2Fstreetviewpixels-pa.googleapis.com%2Fv1%2Fthumbnail%3Fpanoid%3D-3UaGvp-WrUGA56pyk-7dg%26cb_client%3Dsearch.revgeo_and_fetch.gps%26w%3D96%26h%3D64%26yaw%3D337.08377%26pitch%3D0%26thumbfov%3D100!7i16384!8i8192?entry=ttu'>View on Google Maps</a>")
             .openPopup();
 
         // Add a marker for Rem's Pet Shop Roxas
         var marker2 = L.marker([12.5988553, 121.4834413]).addTo(map)
-            .bindPopup("<b>Rem's Pet Shop Roxas</b><br>Located here. <br><a href='https://www.google.com/maps/@12.5988553,121.4834413,3a,75y,161.59h,79.46t/data=!3m7!1e1!3m5!1sbJ_R7zKmjzm0ivwL-ikUTg!2e0!6shttps:%2F%2Fstreetviewpixels-pa.googleapis.com%2Fv1%2Fthumbnail%3Fpanoid%3DbJ_R7zKmjzm0ivwL-ikUTg%26cb_client%3Dmaps_sv.tactile.gps%26w%3D203%26h%3D100%26yaw%3D201.91084%26pitch%3D0%26thumbfov%3D100!7i16384!8i8192?entry=ttu'>View on Google Maps</a>")
+            .bindPopup("<b>Rem's Pet Shop Roxas</b><br>Available for Accomodation <br>Located here. <br><a href='https://www.google.com/maps/@12.5988553,121.4834413,3a,75y,161.59h,79.46t/data=!3m7!1e1!3m5!1sbJ_R7zKmjzm0ivwL-ikUTg!2e0!6shttps:%2F%2Fstreetviewpixels-pa.googleapis.com%2Fv1%2Fthumbnail%3Fpanoid%3DbJ_R7zKmjzm0ivwL-ikUTg%26cb_client%3Dmaps_sv.tactile.gps%26w%3D203%26h%3D100%26yaw%3D201.91084%26pitch%3D0%26thumbfov%3D100!7i16384!8i8192?entry=ttu'>View on Google Maps</a>")
             .openPopup();
 
         // Make the marker for Rem's Pet Shop Roxas clickable
